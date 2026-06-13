799aca8d7bb6f8279f3f2ff1e77d2d30b9a588a3
Ubuntu <ubuntu@ip-172-31-3-173.ap-northeast-1.compute.internal>
2026-06-13 03:34:20 +0000

    chore: complete 2nd deployment and testing

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,6 +10,6 @@ class Admin::UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_users_path, notice: "ユーザーを退会させました"
+    redirect_to admin_users_path, notice: "ユーザーを退会させました", status: :see_other
   end
 end
