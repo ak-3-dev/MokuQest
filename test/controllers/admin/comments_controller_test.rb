411c83cfd94754254eb2979e4fd311859861ff98
Ubuntu <ubuntu@ip-172-31-3-173.ap-northeast-1.compute.internal>
2026-09-16 14:32:42 +0000

    Fix controller tests and fixtures

--- test/controllers/admin/comments_controller_test.rb
+++ test/controllers/admin/comments_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class Admin::CommentsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get index" do
+    sign_in admins(:one)
+
     get admin_comments_index_url
+
     assert_response :success
   end
 end
